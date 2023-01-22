f78327b04bc6300207448c37e19c1fe46db28cf5
Andrew Robling <a@a-macpro.local>
2023-01-22 09:57:08 -0600

    fix(comment): added comment

--- 01-color-flipper/hex.js
+++ 01-color-flipper/hex.js
@@ -1,3 +1,4 @@
+// array containing digits and letters to generate random assortment for hex codes
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 const btn = document.getElementById("btn");
 const Color = document.querySelector(".color");
