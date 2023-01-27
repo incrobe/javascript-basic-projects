309320d0c7003b3a938f57df4aab511352a2c2e1
Andrew Robling <a@a-macpro.local>
2023-01-26 23:27:40 -0600

    fix(index.html): onClick to onclick

--- 02-press-me-button/index.js
+++ 02-press-me-button/index.js
@@ -1,7 +1,7 @@
 const buttonA = document.querySelector("#button_A");
 console.log(buttonA);
 
-buttonA.onClick = () => {
+buttonA.onclick = () => {
   const name = prompt("What is your name?");
   alert(`Hello ${name}!`);
 };
